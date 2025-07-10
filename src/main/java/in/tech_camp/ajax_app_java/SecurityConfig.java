55dbc0cad8cb06399714b26f98bc55fbe7caff64
ponnnn1 <miyawaki.ayana@minebea-ss.com>
2025-07-10 14:38:30 +0900

    Basic認証の設定を追加

---
+++ src/main/java/in/tech_camp/ajax_app_java/SecurityConfig.java
@@ -0,0 +1,50 @@
+package in.tech_camp.ajax_app_java;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig { 
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+        .anyRequest().authenticated()
+      )
+      .formLogin(form -> form
+        .permitAll() 
+      )
+      .httpBasic(httpBasicCustomizer -> { });
+
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    String username = System.getenv("BASIC_AUTH_USER");  //profileに記載した
+    String password = System.getenv("BASIC_AUTH_PASSWORD");  //profileに記載した
+    
+    // UserDetails user = User.withUsername("admin")
+    //     .password(encoder.encode("2222"))
+    UserDetails user = User.withUsername(username)
+        .password(encoder.encode(password))
+        .roles("ADMIN")
+        .build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+  }
+}
